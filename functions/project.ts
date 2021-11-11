5566002e44c0e1c56e3fccf3f5dccd7ed2950d9a
Pablo Suarez <pablo.suarez@imagineapps.co>
2021-11-11 01:04:57 -0500

    GET ALL Projects

--- functions/project.ts
+++ functions/project.ts
@@ -1,14 +1,18 @@
 import { Handler } from "@netlify/functions";
 import Project from '../src/models/Project';
+import '../src/database';
 
 const handler: Handler = async (event, context) => {
   const path: string = event.path;
   const method: string = event.httpMethod;
   const headers: any = event.headers;
 
-  if (path.endsWith("getAll")) {
+  if (path.endsWith("getAll") && method == "GET") {
     try {
       let allProjects = await Project.find({});
+
+      console.log(allProjects);
+
       return({
         statusCode: 200,
         body: JSON.stringify(allProjects)
